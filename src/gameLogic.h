79b18fd1ecf0e4b1a02b45ebe41f6a17753097a9
Idris <uddinmahi156@gmail.com>
2026-07-01 11:18:02 +0530

    Final clean up

--- src/gameLogic.h
+++ src/gameLogic.h
@@ -1,13 +1,10 @@
 #pragma once
 #include <project.h>
 
-
 void switchLed();
 void randLed();
 void ledInit();
-void play();
-void About();
-void highScoreMenue();
+
 void wonMechanics();
 void looseMechanics();
 int calcReactTime();
